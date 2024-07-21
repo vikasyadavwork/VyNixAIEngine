c8ed7f538f756f18f5a78df3b1ca9f40dd35f515
vikasyadav <vikasyadavwork@outlook.com>
2024-07-21 15:51:23 +0530

    add the core header

---
+++ Engine/Core/include/vx/Core/Core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+//==============================================================
+// VyNix Core
+// Common engine includes
+//==============================================================
+
+#include <vx/Core/Types.h>
+#include <vx/Core/Macros.h>
+#include <vx/Core/Platform.h>
+#include <vx/Core/Assert.h>
+#include <vx/Core/Version.h>
+#include <vx/Core/Timer.h>
+#include <vx/Core/Logger.h>
