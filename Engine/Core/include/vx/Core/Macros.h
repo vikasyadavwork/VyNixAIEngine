d628e34b28084f5e6462820932a1e0db5726115e
vikasyadav <vikasyadavwork@outlook.com>
2024-10-15 19:15:47 +0530

    adjust the engine macros

--- Engine/Core/include/vx/Core/Macros.h
+++ Engine/Core/include/vx/Core/Macros.h
@@ -36,22 +36,3 @@
 #else
 #define VX_FORCE_INLINE inline __attribute__((always_inline))
 #endif
-
-//Logging
-#define VX_LOG_TRACE(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Trace, msg)
-
-#define VX_LOG_DEBUG(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Debug, msg)
-
-#define VX_LOG_INFO(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Info, msg)
-
-#define VX_LOG_WARN(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Warn, msg)
-
-#define VX_LOG_ERROR(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Error, msg)
-
-#define VX_LOG_FATAL(msg) \
-    ::vx::Logger::Get().Log(::vx::LogLevel::Fatal, msg)
