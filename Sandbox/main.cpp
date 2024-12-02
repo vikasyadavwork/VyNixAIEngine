0834d1a847e41fa717f5bc1db760b8f2fb73cc67
vikasyadav <vikasyadavwork@outlook.com>
2024-12-02 12:15:10 +0530

    start the application from the sandbox

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,16 +1,9 @@
-#include <vx/Core/Core.h>
+#include <vx/Core/Application.h>
 
 int main()
 {
-    VX_LOG_TRACE("Starting Engine");
+    vx::Application app;
+    app.Run();
 
-    VX_LOG_DEBUG("Loading Renderer");
-
-    VX_LOG_INFO("Engine Initialized");
-
-    VX_LOG_WARN("Texture Missing");
-
-    VX_LOG_ERROR("Failed to Load Shader");
-
-    VX_LOG_FATAL("GPU Device Lost");
+    return 0;
 }
