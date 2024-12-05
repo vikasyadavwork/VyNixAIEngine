5cea8bb78585e8ed5c3fadc208f3e57865877b6e
vikasyadav <vikasyadavwork@outlook.com>
2024-12-05 20:42:55 +0530

    add the main application loop

--- Engine/Core/src/Application.cpp
+++ Engine/Core/src/Application.cpp
@@ -1,19 +1,45 @@
 #include <vx/Core/Application.h>
 #include <vx/Core/Log.h>
+#include <Windows.h>
 
 namespace vx
 {
 
-    Application::Application()
-    {
-        m_Window = Window::Create();
-    }
-
-    Application::~Application() = default;
-
-    void Application::Run()
-    {
-        VX_LOG_INFO("VyNix AI Engine Started!");
-    }
-
+Application::Application()
+{
+    m_Window = Window::Create();
 }
+
+Application::~Application() = default;
+
+void Application::Run()
+{
+VX_LOG_INFO("VyNix AI Engine Started!");
+while (m_Running) {
+    ProcessMessages();
+}
+}
+
+void Application::Close()
+{
+    VX_LOG_INFO("VyNix AI Engine Closed!");
+    m_Running = false;
+}
+
+void Application::ProcessMessages()
+{
+    MSG msg{};
+
+    while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+    {
+        if (msg.message == WM_QUIT) {
+            m_Running = false;
+            break;
+        }
+
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
+
+} // namespace vx
