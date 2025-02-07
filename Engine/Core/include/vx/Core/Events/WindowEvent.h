21e25f0f15a21756b1c3f14c5f26341a47d8ffdf
vikasyadav <vikasyadavwork@outlook.com>
2025-02-07 20:54:49 +0530

    add resize and focus events

--- Engine/Core/include/vx/Core/Events/WindowEvent.h
+++ Engine/Core/include/vx/Core/Events/WindowEvent.h
@@ -1,15 +1,17 @@
 #pragma once
 #include <vx/Core/Event.h>
 
-namespace vx {
+namespace vx
+{
 class WindowEvent : public Event
 {
-public:
+  public:
     virtual ~WindowEvent() = default;
 };
 
-class WindowCloseEvent : public WindowEvent {
-public:
+class WindowCloseEvent : public WindowEvent
+{
+  public:
     EventType GetEventType() const override
     {
         return EventType::WindowClose;
@@ -19,4 +21,45 @@ public:
         return "WindowCloseEvent";
     }
 };
-}
+
+class WindowResizeEvent : public WindowEvent
+{
+  public:
+    WindowResizeEvent(unsigned width, unsigned height) : Width(width), Height(height) {}
+    unsigned Width, Height;
+    unsigned GetWidth() const noexcept
+    {
+        return Width;
+    }
+    unsigned GetHeight() const noexcept
+    {
+        return Height;
+    }
+    EventType GetEventType() const override
+    {
+        return EventType::WindowResize;
+    }
+    std::string_view GetName() const override
+    {
+        return "WindowResizeEvent";
+    }
+};
+class WindowFocusEvent : public WindowEvent
+{
+  public:
+    explicit WindowFocusEvent(bool focused) : Focused(focused) {}
+    bool Focused;
+    bool IsFocused() const noexcept
+    {
+        return Focused;
+    }
+    EventType GetEventType() const override
+    {
+        return EventType::WindowFocus;
+    }
+    std::string_view GetName() const override
+    {
+        return "WindowFocusEvent";
+    }
+};
+} // namespace vx
