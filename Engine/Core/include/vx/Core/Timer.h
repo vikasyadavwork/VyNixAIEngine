89a9239febf3a4792edd10acce512250df410903
vikasyadav <vikasyadavwork@outlook.com>
2025-02-23 20:09:08 +0530

    update the timer interface

--- Engine/Core/include/vx/Core/Timer.h
+++ Engine/Core/include/vx/Core/Timer.h
@@ -5,6 +5,19 @@ namespace vx
 
 class Timer
 {
+  public:
+    Timer();
+
+    void Reset();
+    void Update();
+
+    float GetDeltaTime() const noexcept;
+    float GetElapsedTime() const noexcept;
+
+  private:
+    float m_DeltaTime;
+    float m_ElapsedTime;
+    double m_LastTime;
 };
 
-}
+} // namespace vx
