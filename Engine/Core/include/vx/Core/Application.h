44a96cffd1b25235d02f822774cabb62e72adc6d
vikasyadav <vikasyadavwork@outlook.com>
2025-02-26 22:14:08 +0530

    add layer support to the application

--- Engine/Core/include/vx/Core/Application.h
+++ Engine/Core/include/vx/Core/Application.h
@@ -1,21 +1,40 @@
 #pragma once
 #include <memory>
+#include <vx/Core/Layer.h>
+#include <vx/Core/LayerStack.h>
+#include <vx/Core/Timer.h>
 #include <vx/Core/Window.h>
 
 namespace vx
 {
 class Application
 {
-public:
-    Application();
+  public:
+    explicit Application(const WindowProps& props = {});
     virtual ~Application();
 
-    void Run();
+    void Run(uint64_t maxFrames = 0);
     void Close();
-private:
-    void ProcessMessages();
-private:
+    Window& GetWindow() noexcept
+    {
+        return *m_Window;
+    }
+    const Window& GetWindow() const noexcept
+    {
+        return *m_Window;
+    }
+
+    // Layer
+    void PushLayer(Layer* layer);
+    void PushOverlay(Layer* overlay);
+
+  private:
+    void OnEvent(Event& event);
+
+  private:
     bool m_Running = true;
     std::unique_ptr<Window> m_Window;
+    LayerStack m_LayerStack;
+    Timer m_Timer;
 };
-}
+} // namespace vx
