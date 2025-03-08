b56659575f6329f03875f7e2520abf1f02a5c203
vikasyadav <vikasyadavwork@outlook.com>
2025-03-08 20:22:21 +0530

    make input available from the core header

--- Engine/Core/include/vx/Core/Core.h
+++ Engine/Core/include/vx/Core/Core.h
@@ -6,10 +6,11 @@
 //==============================================================
 
 #include <vx/Core/Application.h>
-#include <vx/Core/Types.h>
+#include <vx/Core/Assert.h>
+#include <vx/Core/Input.h>
+#include <vx/Core/Logger.h>
 #include <vx/Core/Macros.h>
 #include <vx/Core/Platform.h>
-#include <vx/Core/Assert.h>
-#include <vx/Core/Version.h>
 #include <vx/Core/Timer.h>
-#include <vx/Core/Logger.h>
+#include <vx/Core/Types.h>
+#include <vx/Core/Version.h>
