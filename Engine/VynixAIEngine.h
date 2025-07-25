817f10f0cdd80e58aabe7bad557ace878790fe4c
vikasyadav <vikasyadavwork@outlook.com>
2025-07-25 21:18:21 +0530

    update the main engine header

--- Engine/VynixAIEngine.h
+++ Engine/VynixAIEngine.h
@@ -1,4 +1,10 @@
 #pragma once
 
-// Core
-#include "Core/Core.h"
+// Add Engine/ to the include path when using this convenience umbrella.
+#include <vx/Audio/Audio.h>
+#include <vx/Core/Application.h>
+#include <vx/Core/Input.h>
+#include <vx/Core/Layer.h>
+#include <vx/Core/Timer.h>
+#include <vx/Renderer/Renderer2D.h>
+#include <vx/Scene/Scene.h>
